83589e63edd837e34c91fef95866a74772497c2a
vvarsema-prog <vvarsema@gmail.com>
2026-05-22 11:49:15 +0400

    animals

--- js/script.js
+++ js/script.js
@@ -1,11 +1,48 @@
-console.log(document.querySelectorAll(".animal"));
+let animals = document.querySelectorAll(".animal");
 
-document.querySelector("p").addEventListener('clck',function(){
-    
-});
-let animals = document.querySelectorAll("animal");
-for(i = 0; i < animals.length; i++ ){
-    animals.addEventListener('click',function(){
-        alert("damakliket");
-    })
+
+document.addEventListener('keydown',function (e) {
+     let soundsrc
+     if (e.key == "C" || e.key == "c") {
+            soundsrc = "assets/audio/caw.wav"
+        }
+        else if(e.key == "D" || e.key == "d"){
+            let soundsrc = "assets/audio/dog.wav"
+        }
+        else if(e.key == "J" || e.key == "j"){
+            let soundsrc = "assets/audio/saddonkey.wav"
+        }
+        else if(e.key == "L" || e.key == "l"){
+            let soundsrc = "assets/audio/lion.wav"
+        }
+        let sound = new Audio(soundsrc);
+            sound.play();
+})
+// davamatebt || ert if elsshi
+
+function playsound(leter){
+    let soundsrc
+     if (leter == "C") {
+            soundsrc = "assets/audio/caw.wav"
+        }
+        else if(leter == "D"){
+            let soundsrc = "assets/audio/dog.wav"
+        }
+        else if(leter == "J"){
+            let soundsrc = "assets/audio/saddonkey.wav"
+        }
+        else if(leter == "L"){
+            let soundsrc = "assets/audio/lion.wav"
+        }
+        let sound = new Audio(soundsrc);
+            sound.play();
+    }
+
+
+for(let i = 0; i < animals.length; i++){
+    console.log(animals[i]);
+    animals[i].addEventListener("click", function(){
+        console.log(animals[i]);
+       playsound(animals[i].innerHTML);
+})
 }
--- js/test.js
+++ js/test.js
@@ -6,3 +6,27 @@ let housekeeper = {
 
 console.log(housekeeper.name+"s uyvars shemdegi otaxebis dalageba: "+housekeeper.favoriteroomlageba);
 
+// let bellboy ={
+//     name: "dali",
+//     exp: "30",
+//     favoriteroomlageba: ["48", "samzareulo"],
+//     gamodzaxeba: function () {
+//         alert("movedi")
+//     }
+// }
+// let bellboy2 ={
+//     name: "givi",
+//     exp: "302",
+//     favoriteroomlageba: ["484", "samzareuloi"]
+// }
+function Housekeeper (housekeepername,housekeeperexperance, housekeeperoom) {
+    this.name = housekeepername;
+    this.exp = housekeeperexperance;
+    this.favoriteroomlageba = housekeeperoom;
+}
+let housekeeper2 = new Housekeeper("dali",302,30);
+// console.log(housekeeper2);
+
+document.addEventListener('keydown',function (e) {
+    
+})
